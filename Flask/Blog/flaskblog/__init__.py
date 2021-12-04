966d92c0894c6620f30eeeae629bf5b2379dbbbc
ii-ko <mhdihsan90@gmail.com>
2021-12-04 16:42:20 +0700

    add flask_migrate

--- Flask/Blog/flaskblog/__init__.py
+++ Flask/Blog/flaskblog/__init__.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+
+app.config['SECRET_KEY'] = 'secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/flaskblog'
 
 from flaskblog.routes import routes
 
