c55b0ad2c08f9e1a9e8966505e8bdbf701e120a9
ii-ko <mhdihsan90@gmail.com>
2021-12-06 20:01:44 +0700

    update flask/blog
    add templates
    show data

--- Flask/Blog/main.py
+++ Flask/Blog/main.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+posts = [{
+    'author': 'ii-ko',
+    'title': 'programming',
+    'content': 'Python programming',
+    'date_posted': 'May 3rd, 2012'
+},
+{
+    'author': 'tamae',
+    'title': 'design',
+    'content': 'Web design',
+    'date_posted': 'May 4th, 2012'
+},
+]
 
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return render_template('index.html', posts=posts)
 
 
 if __name__ == '__main__':
