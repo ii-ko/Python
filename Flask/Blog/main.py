3e9c94c14887a1a7ca72a7536895d02ff7ee5547
ii-ko <mhdihsan90@gmail.com>
2021-12-06 20:10:00 +0700

    update flask/blog
    add bootstrap

--- Flask/Blog/main.py
+++ Flask/Blog/main.py
@@ -19,12 +19,12 @@ posts = [{
 
 @app.route('/')
 def index():
-    return render_template('index.html', posts=posts)
+    return render_template('index.html', posts=posts, title='Home')
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
