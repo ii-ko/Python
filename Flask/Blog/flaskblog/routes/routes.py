f03b0f81753e19f8cab136faa29cb7f968ff43f1
ii-ko <mhdihsan90@gmail.com>
2021-12-07 08:57:40 +0700

    update flask/blog
    prevent access login & register form if user is authenticated

--- Flask/Blog/flaskblog/routes/routes.py
+++ Flask/Blog/flaskblog/routes/routes.py
@@ -37,6 +37,8 @@ def about():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -50,6 +52,8 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
