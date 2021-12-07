06d1e75a2b765bbd4f67801a7a0fa2d8c5613292
ii-ko <mhdihsan90@gmail.com>
2021-12-07 19:30:33 +0700

    update flask/blog
    add pagination

--- Flask/Blog/flaskblog/routes/routes.py
+++ Flask/Blog/flaskblog/routes/routes.py
@@ -12,7 +12,8 @@ import secrets
 
 @app.route('/')
 def home():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=4)
     return render_template('pages/index.html', posts=posts, title='Home')
 
 
