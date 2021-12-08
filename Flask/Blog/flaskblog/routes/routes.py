8a61d63f87c3596b35c07163d22bfdf0d309fcbc
ii-ko <mhdihsan90@gmail.com>
2021-12-08 16:07:35 +0700

    update flask/blog
    create update function

--- Flask/Blog/flaskblog/routes/routes.py
+++ Flask/Blog/flaskblog/routes/routes.py
@@ -60,6 +60,27 @@ def delete_post(post_id):
     return redirect(url_for('home'))
 
 
+@app.route('/post/<int:post_id>/update', methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.description = form.desc.data
+        post.content = form.content.data
+        db.session.commit()
+        flash(f'Your Post has been updated!', 'success')
+        return redirect(url_for('read_more_post', post_id=post_id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.desc.data = post.description
+        form.content.data = post.content
+    return render_template('posts/new_post.html', title='Update Post', form=form, legend='Update Post')
+
+
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
