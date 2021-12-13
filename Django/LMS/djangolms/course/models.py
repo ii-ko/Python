c85861a5167f7544697576e8ce8c7954912ad315
ii-ko <mhdihsan90@gmail.com>
2021-12-13 19:19:26 +0700

    update django/lms
    django:
    Create model Course

--- Django/LMS/djangolms/course/models.py
+++ Django/LMS/djangolms/course/models.py
@@ -8,3 +8,12 @@ class Category(models.Model):
     slug = models.SlugField()
     short_description = models.TextField(blank=True, null=True)
     created_at = models.DateField(auto_now_add=True)
+
+
+class Course(models.Model):
+    categories = models.ManyToManyField(Category)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    short_description = models.TextField(blank=True, null=True)
+    long_description = models.TextField(blank=True, null=True)
+    created_at = models.DateField(auto_now_add=True)
