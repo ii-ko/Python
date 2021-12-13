748bb9b4f4621984fb71ae2d06a067d60b8a6e7f
ii-ko <mhdihsan90@gmail.com>
2021-12-13 11:44:51 +0700

    update django/lms
    config vuex

--- Django/LMS/djangolms/vuelms/src/App.vue
+++ Django/LMS/djangolms/vuelms/src/App.vue
@@ -8,12 +8,24 @@
 <script>
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import axios from "axios";
 
 export default {
   name: "App",
   components: {
     Navbar,
     Footer,
+  },
+  befroreCreate() {
+    this.$store.commit("initializeStore");
+
+    const token = this.$store.state.user.token;
+
+    if (token) {
+      axios.defaults.headers.common["Authorization"] = "Token " + token;
+    } else {
+      axios.defaults.headers.common["Authorization"] = "";
+    }
   },
 };
 </script>
--- Django/LMS/djangolms/vuelms/src/store/index.js
+++ Django/LMS/djangolms/vuelms/src/store/index.js
@@ -1,12 +1,33 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    user: {
+      token: "",
+      isAuthenticated: false,
+    },
   },
   mutations: {
+    // function init store
+    initializeStore(state) {
+      if (localStorage.getItem("token")) {
+        state.user.token = localStorage.getItem("token");
+        state.user.isAuthenticated = true;
+      } else {
+        (state.user.token = ""), (state.user.isAuthenticated = false);
+      }
+    },
+    // function set token
+    setToken(state, token) {
+      state.user.token = token;
+      state.user.isAuthenticated = true;
+    },
+    // function hapus token
+    removeToken(state) {
+      state.user.token = "";
+      state.user.isAuthenticated = false;
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
