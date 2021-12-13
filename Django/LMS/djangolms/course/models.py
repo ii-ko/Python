474083e2a92d55f407f66ece4c9ac2cea755861e
ii-ko <mhdihsan90@gmail.com>
2021-12-13 19:17:32 +0700

    update django/lms
    django:
    create category model

--- Django/LMS/djangolms/course/models.py
+++ Django/LMS/djangolms/course/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    short_description = models.TextField(blank=True, null=True)
+    created_at = models.DateField(auto_now_add=True)
